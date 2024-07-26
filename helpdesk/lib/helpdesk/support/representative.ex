5eff20446185484748c97daa4597475aafbd84a4
Sangeetha <sangeethailango21@gmail.com>
2024-07-26 09:23:33 +0530

    accepting only non nil values

--- helpdesk/lib/helpdesk/support/representative.ex
+++ helpdesk/lib/helpdesk/support/representative.ex
@@ -4,45 +4,47 @@ defmodule Helpdesk.Support.Representative do
     data_layer: AshPostgres.DataLayer
 
   postgres do
-    table "representatives"
-    repo Helpdesk.Repo
+    table("representatives")
+    repo(Helpdesk.Repo)
   end
 
   actions do
-    defaults [:read]
+    defaults([:read])
 
     create :create do
-      accept [:name, :num]
+      accept([:name, :num])
     end
   end
 
   attributes do
-    uuid_primary_key :id
+    uuid_primary_key(:id)
 
     attribute :name, :string do
-      public? true
+      public?(true)
     end
 
-    attribute :num, :integer
+    attribute :num, :integer do
+      allow_nil?(false)
+    end
   end
 
   aggregates do
     exists :subject_issue_6, :tickets do
-      filter expr(subject == "Issue 4")
+      filter(expr(subject == "Issue 4"))
     end
 
-    count :count_of_tickets, :tickets
+    count(:count_of_tickets, :tickets)
 
     list :list_of_open_tickets, :tickets, :status do
-      filter expr(status == "open")
+      filter(expr(status == "open"))
     end
 
     first :matching_value_for_subject, :tickets, :status do
-      filter expr(like(status, "closed"))
+      filter(expr(like(status, "closed")))
     end
   end
 
   relationships do
-    has_many :tickets, Helpdesk.Support.Ticket
+    has_many(:tickets, Helpdesk.Support.Ticket)
   end
 end
